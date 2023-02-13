c5bf4b21e7a0a826f3da800cca0cda7231f6f859
Jean-Luc Guermond <guermond@math.tamu.edu>
2023-02-13 09:46:46 -0600

    jlg

--- GAUSS_POINTS_OBJET/GP_2D_p3.f90
+++ GAUSS_POINTS_OBJET/GP_2D_p3.f90
@@ -135,126 +135,125 @@ CONTAINS
        d(1, 10, j)=df10x(xx(j), yy(j))
        d(2, 10, j)=df10y(xx(j), yy(j))
     ENDDO
-
   END SUBROUTINE element_2d_p3
 
- SUBROUTINE element_2d_p3_boundary (face, d, w, n_w, l_Gs)
-   !===triangular element with cubic interpolation
-   !===and seven Gauss integration points
-   !===d(2, n_w, l_G) : derivatives values of shape functions at Gauss points
-   IMPLICIT NONE
-   INTEGER,                                INTENT(IN)  :: n_w, l_Gs
-   INTEGER,                                INTENT(IN)  :: face
-   REAL(KIND=8), DIMENSION(2, n_w,  l_Gs), INTENT(OUT) :: d
-   REAL(KIND=8), DIMENSION(   n_w, l_Gs),  INTENT(OUT) :: w
-   REAL(KIND=8), DIMENSION(l_Gs)                       :: xx, yy, gp
-   INTEGER :: j, l
-   REAL(KIND=8) :: half=0.5d0
-   REAL(KIND=8) ::  f1, f2, f3, f4, f5, f6, f7, f8, f9, f10,   &
-        df1x, df2x, df3x, df4x, df5x, df6x, df7x, df8x, df9x, df10x, &
-        df1y, df2y, df3y, df4y, df5y, df6y, df7y, df8y, df9y, df10y, &
-        x, y
+  SUBROUTINE element_2d_p3_boundary (face, d, w, n_w, l_Gs)
+    !===triangular element with cubic interpolation
+    !===and seven Gauss integration points
+    !===d(2, n_w, l_G) : derivatives values of shape functions at Gauss points
+    IMPLICIT NONE
+    INTEGER,                                INTENT(IN)  :: n_w, l_Gs
+    INTEGER,                                INTENT(IN)  :: face
+    REAL(KIND=8), DIMENSION(2, n_w,  l_Gs), INTENT(OUT) :: d
+    REAL(KIND=8), DIMENSION(   n_w, l_Gs),  INTENT(OUT) :: w
+    REAL(KIND=8), DIMENSION(l_Gs)                       :: xx, yy, gp
+    INTEGER :: j, l
+    REAL(KIND=8) :: half=0.5d0
+    REAL(KIND=8) ::  f1, f2, f3, f4, f5, f6, f7, f8, f9, f10,   &
+         df1x, df2x, df3x, df4x, df5x, df6x, df7x, df8x, df9x, df10x, &
+         df1y, df2y, df3y, df4y, df5y, df6y, df7y, df8y, df9y, df10y, &
+         x, y
 
-   f1(x,y) = -0.9d1/0.2d1*x**3 - 0.27d2/0.2d1*x**2*y - 0.27d2/0.2d1*x*y**2 &
-        - 0.9d1/0.2d1*y**3 + 0.9d1*x**2 + 0.18d2*x*y + 0.9d1*y**2 &
-        - 0.11d2/0.2d1*x - 0.11d2/0.2d1*y + 0.1d1
-   f2(x,y) = 0.9d1/0.2d1*x**3 - 0.9d1/0.2d1*x**2 + x
-   f3(x,y) = 0.9d1/0.2d1*y**3 - 0.9d1/0.2d1*y**2 + y
-   f4(x,y) = 0.27d2/0.2d1*x**2*y - 0.9d1/0.2d1*x*y
-   f5(x,y) = 0.27d2/0.2d1*x*y**2 - 0.9d1/0.2d1*x*y
-   f6(x,y) = 0.27d2/0.2d1*x**2*y + 0.27d2*x*y**2 + 0.27d2/0.2d1*y**3 &
-        - 0.45d2/0.2d1*x*y - 0.45d2/0.2d1*y**2 + 0.9d1*y
-   f7(x,y) = -0.27d2/0.2d1*x*y**2 - 0.27d2/0.2d1*y**3 + 0.9d1/0.2d1*x*y &
-        + 0.18d2*y**2 - 0.9d1/0.2d1*y
-   f8(x,y) = 0.27d2/0.2d1*x**3 + 0.27d2*x**2*y + 0.27d2/0.2d1*x*y**2 &
-        - 0.45d2/0.2d1*x**2 - 0.45d2/0.2d1*x*y + 0.9d1*x
-   f9(x,y) = -0.27d2/0.2d1*x**3 - 0.27d2/0.2d1*x**2*y + 0.18d2*x**2 &
-        + 0.9d1/0.2d1*x*y - 0.9d1/0.2d1*x
-   f10(x,y) = -27*x**2*y - 27*x*y**2 + 27*x*y
+    f1(x,y) = -0.9d1/0.2d1*x**3 - 0.27d2/0.2d1*x**2*y - 0.27d2/0.2d1*x*y**2 &
+         - 0.9d1/0.2d1*y**3 + 0.9d1*x**2 + 0.18d2*x*y + 0.9d1*y**2 &
+         - 0.11d2/0.2d1*x - 0.11d2/0.2d1*y + 0.1d1
+    f2(x,y) = 0.9d1/0.2d1*x**3 - 0.9d1/0.2d1*x**2 + x
+    f3(x,y) = 0.9d1/0.2d1*y**3 - 0.9d1/0.2d1*y**2 + y
+    f4(x,y) = 0.27d2/0.2d1*x**2*y - 0.9d1/0.2d1*x*y
+    f5(x,y) = 0.27d2/0.2d1*x*y**2 - 0.9d1/0.2d1*x*y
+    f6(x,y) = 0.27d2/0.2d1*x**2*y + 0.27d2*x*y**2 + 0.27d2/0.2d1*y**3 &
+         - 0.45d2/0.2d1*x*y - 0.45d2/0.2d1*y**2 + 0.9d1*y
+    f7(x,y) = -0.27d2/0.2d1*x*y**2 - 0.27d2/0.2d1*y**3 + 0.9d1/0.2d1*x*y &
+         + 0.18d2*y**2 - 0.9d1/0.2d1*y
+    f8(x,y) = 0.27d2/0.2d1*x**3 + 0.27d2*x**2*y + 0.27d2/0.2d1*x*y**2 &
+         - 0.45d2/0.2d1*x**2 - 0.45d2/0.2d1*x*y + 0.9d1*x
+    f9(x,y) = -0.27d2/0.2d1*x**3 - 0.27d2/0.2d1*x**2*y + 0.18d2*x**2 &
+         + 0.9d1/0.2d1*x*y - 0.9d1/0.2d1*x
+    f10(x,y) = -27*x**2*y - 27*x*y**2 + 27*x*y
 
-   df1x(x,y) = -0.27d2/0.2d1*x**2 - 0.27d2*x*y - 0.27d2/0.2d1*y**2 &
-        + 0.18d2*x + 0.18d2*y - 0.11d2/0.2d1
-   df2x(x,y) = 0.27d2/0.2d1*x**2 - 0.9d1*x + 0.1d1
-   df3x(x,y) = 0
-   df4x(x,y) = 27*x*y - 0.9d1/0.2d1 *y
-   df5x(x,y) = 0.27d2/0.2d1*y**2 - 0.9d1/0.2d1*y
-   df6x(x,y) = 27*x*y + 27*y**2 - 0.45d2/0.2d1*y
-   df7x(x,y) = -0.27d2/0.2d1*y**2 + 0.9d1/0.2d1*y
-   df8x(x,y) = 0.81d2/0.2d1*x**2 + 0.54d2*x*y - 0.45d2*x &
-        + 0.27d2/0.2d1*y**2 - 0.45d2/0.2d1*y + 0.9d1
-   df9x(x,y) = -0.81d2/0.2d1*x**2 + 0.36d2*x - 0.27d2*x*y &
-        + 0.9d1/0.2d1*y - 0.9d1/0.2d1
-   df10x(x,y) = -54*x*y - 27*y**2 + 27*y
+    df1x(x,y) = -0.27d2/0.2d1*x**2 - 0.27d2*x*y - 0.27d2/0.2d1*y**2 &
+         + 0.18d2*x + 0.18d2*y - 0.11d2/0.2d1
+    df2x(x,y) = 0.27d2/0.2d1*x**2 - 0.9d1*x + 0.1d1
+    df3x(x,y) = 0
+    df4x(x,y) = 27*x*y - 0.9d1/0.2d1 *y
+    df5x(x,y) = 0.27d2/0.2d1*y**2 - 0.9d1/0.2d1*y
+    df6x(x,y) = 27*x*y + 27*y**2 - 0.45d2/0.2d1*y
+    df7x(x,y) = -0.27d2/0.2d1*y**2 + 0.9d1/0.2d1*y
+    df8x(x,y) = 0.81d2/0.2d1*x**2 + 0.54d2*x*y - 0.45d2*x &
+         + 0.27d2/0.2d1*y**2 - 0.45d2/0.2d1*y + 0.9d1
+    df9x(x,y) = -0.81d2/0.2d1*x**2 + 0.36d2*x - 0.27d2*x*y &
+         + 0.9d1/0.2d1*y - 0.9d1/0.2d1
+    df10x(x,y) = -54*x*y - 27*y**2 + 27*y
 
-   df1y(x,y) = -0.27d2/0.2d1*x**2 - 0.27d2*x*y - 0.27d2/0.2d1*y**2 &
-        + 0.18d2*x + 0.18d2*y - 0.11d2/0.2d1
-   df2y(x,y) = 0.d0
-   df3y(x,y) = 0.27d2/0.2d1*y**2 - 0.9d1*y + 0.1d1
-   df4y(x,y) = 0.27d2/0.2d1*x**2 - 0.9d1/0.2d1*x
-   df5y(x,y) = 27*x*y - 0.9d1/0.2d1*x
-   df6y(x,y) = 54*x*y + 0.81d2/0.2d1*y**2 &
-        - 45*y + 0.27d2/0.2d1*x**2 - 0.45d2/0.2d1*x + 0.9d1
-   df7y(x,y) = -0.81d2/0.2d1*y**2 + 0.36d2*y - 0.27d2*x*y + 0.9d1/0.2d1*x - 0.9d1/0.2d1
-   df8y(x,y) = 27*x**2 + 27*x*y - 0.45d2/0.2d1*x
-   df9y(x,y) = -0.27d2/0.2d1*x**2 + 0.9d1/0.2d1*x
-   df10y(x,y) = -27*x**2 - 54*x*y + 27*x
+    df1y(x,y) = -0.27d2/0.2d1*x**2 - 0.27d2*x*y - 0.27d2/0.2d1*y**2 &
+         + 0.18d2*x + 0.18d2*y - 0.11d2/0.2d1
+    df2y(x,y) = 0.d0
+    df3y(x,y) = 0.27d2/0.2d1*y**2 - 0.9d1*y + 0.1d1
+    df4y(x,y) = 0.27d2/0.2d1*x**2 - 0.9d1/0.2d1*x
+    df5y(x,y) = 27*x*y - 0.9d1/0.2d1*x
+    df6y(x,y) = 54*x*y + 0.81d2/0.2d1*y**2 &
+         - 45*y + 0.27d2/0.2d1*x**2 - 0.45d2/0.2d1*x + 0.9d1
+    df7y(x,y) = -0.81d2/0.2d1*y**2 + 0.36d2*y - 0.27d2*x*y + 0.9d1/0.2d1*x - 0.9d1/0.2d1
+    df8y(x,y) = 27*x**2 + 27*x*y - 0.45d2/0.2d1*x
+    df9y(x,y) = -0.27d2/0.2d1*x**2 + 0.9d1/0.2d1*x
+    df10y(x,y) = -27*x**2 - 54*x*y + 27*x
 
-   gp(1) = -SQRT((15.d0+2*SQRT(30.d0))/35.d0)
-   gp(2) = -SQRT((15.d0-2*SQRT(30.d0))/35.d0)
-   gp(3) =  SQRT((15.d0-2*SQRT(30.d0))/35.d0)
-   gp(4) =  SQRT((15.d0+2*SQRT(30.d0))/35.d0)
+    gp(1) = -SQRT((15.d0+2*SQRT(30.d0))/35.d0)
+    gp(2) = -SQRT((15.d0-2*SQRT(30.d0))/35.d0)
+    gp(3) =  SQRT((15.d0-2*SQRT(30.d0))/35.d0)
+    gp(4) =  SQRT((15.d0+2*SQRT(30.d0))/35.d0)
 
-   IF (face==1) THEN
-      DO l = 1, l_Gs
-         xx(l) = 1.d0-(gp(l)+1.d0)*half
-         yy(l) = 0.d0+(gp(l)+1.d0)*half
-      END DO
-   ELSE IF (face==2) THEN
-      DO l = 1, l_Gs
-         xx(l) = 0.d0
-         yy(l) = (gp(l)+1.d0)*half
-      END DO
-   ELSE
-      DO l = 1, l_Gs
-         xx(l) = (gp(l)+1.d0)*half
-         yy(l) = 0.d0
-      END DO
-   END IF
+    IF (face==1) THEN
+       DO l = 1, l_Gs
+          xx(l) = 1.d0-(gp(l)+1.d0)*half
+          yy(l) = 0.d0+(gp(l)+1.d0)*half
+       END DO
+    ELSE IF (face==2) THEN
+       DO l = 1, l_Gs
+          xx(l) = 0.d0
+          yy(l) = (gp(l)+1.d0)*half
+       END DO
+    ELSE
+       DO l = 1, l_Gs
+          xx(l) = (gp(l)+1.d0)*half
+          yy(l) = 0.d0
+       END DO
+    END IF
 
-   DO j = 1, l_Gs
-      w(1, j)    =   f1(xx(j), yy(j))
-      d(1, 1, j) = df1x(xx(j), yy(j))
-      d(2, 1, j) = df1y(xx(j), yy(j))
-      w(2, j)    =   f2(xx(j), yy(j))
-      d(1, 2, j) = df2x(xx(j), yy(j))
-      d(2, 2, j) = df2y(xx(j), yy(j))
-      w(3, j)    =   f3(xx(j), yy(j))
-      d(1, 3, j) = df3x(xx(j), yy(j))
-      d(2, 3, j) = df3y(xx(j), yy(j))
-      w(4, j)    =   f4(xx(j), yy(j))
-      d(1, 4, j) = df4x(xx(j), yy(j))
-      d(2, 4, j) = df4y(xx(j), yy(j))
-      w(5, j)    =   f5(xx(j), yy(j))
-      d(1, 5, j) = df5x(xx(j), yy(j))
-      d(2, 5, j) = df5y(xx(j), yy(j))
-      w(6, j)    =   f6(xx(j), yy(j))
-      d(1, 6, j) = df6x(xx(j), yy(j))
-      d(2, 6, j) = df6y(xx(j), yy(j))
-      w(7, j)    =   f7(xx(j), yy(j))
-      d(1, 7, j) = df7x(xx(j), yy(j))
-      d(2, 7, j) = df7y(xx(j), yy(j))
-      w(8, j)    =   f8(xx(j), yy(j))
-      d(1, 8, j) = df8x(xx(j), yy(j))
-      d(2, 8, j) = df8y(xx(j), yy(j))
-      w(9, j)    =   f9(xx(j), yy(j))
-      d(1, 9, j) = df9x(xx(j), yy(j))
-      d(2, 9, j) = df9y(xx(j), yy(j))
-      w(10, j)   =  f10(xx(j), yy(j))
-      d(1, 10, j)=df10x(xx(j), yy(j))
-      d(2, 10, j)=df10y(xx(j), yy(j))
-   ENDDO
+    DO j = 1, l_Gs
+       w(1, j)    =   f1(xx(j), yy(j))
+       d(1, 1, j) = df1x(xx(j), yy(j))
+       d(2, 1, j) = df1y(xx(j), yy(j))
+       w(2, j)    =   f2(xx(j), yy(j))
+       d(1, 2, j) = df2x(xx(j), yy(j))
+       d(2, 2, j) = df2y(xx(j), yy(j))
+       w(3, j)    =   f3(xx(j), yy(j))
+       d(1, 3, j) = df3x(xx(j), yy(j))
+       d(2, 3, j) = df3y(xx(j), yy(j))
+       w(4, j)    =   f4(xx(j), yy(j))
+       d(1, 4, j) = df4x(xx(j), yy(j))
+       d(2, 4, j) = df4y(xx(j), yy(j))
+       w(5, j)    =   f5(xx(j), yy(j))
+       d(1, 5, j) = df5x(xx(j), yy(j))
+       d(2, 5, j) = df5y(xx(j), yy(j))
+       w(6, j)    =   f6(xx(j), yy(j))
+       d(1, 6, j) = df6x(xx(j), yy(j))
+       d(2, 6, j) = df6y(xx(j), yy(j))
+       w(7, j)    =   f7(xx(j), yy(j))
+       d(1, 7, j) = df7x(xx(j), yy(j))
+       d(2, 7, j) = df7y(xx(j), yy(j))
+       w(8, j)    =   f8(xx(j), yy(j))
+       d(1, 8, j) = df8x(xx(j), yy(j))
+       d(2, 8, j) = df8y(xx(j), yy(j))
+       w(9, j)    =   f9(xx(j), yy(j))
+       d(1, 9, j) = df9x(xx(j), yy(j))
+       d(2, 9, j) = df9y(xx(j), yy(j))
+       w(10, j)   =  f10(xx(j), yy(j))
+       d(1, 10, j)=df10x(xx(j), yy(j))
+       d(2, 10, j)=df10y(xx(j), yy(j))
+    ENDDO
 
- END SUBROUTINE element_2d_p3_boundary
+  END SUBROUTINE element_2d_p3_boundary
 
  SUBROUTINE element_1d_p3(w, d, p, n_ws, l_Gs)
    !===one-dimensional element with cubic interpolation
